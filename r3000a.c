2628ec874f5722b0e363f34d0da37f9a4f007e25
xjsxjs197 <31617363+xjsxjs197@users.noreply.github.com>
2022-07-21 15:39:28 +0800

    Update r3000a.c

--- r3000a.c
+++ r3000a.c
@@ -236,12 +236,12 @@ void psxBranchTest() {
 			}
 		}
 
-		//if (psxRegs.interrupt & 0x80000000) {
-		//	psxRegs.interrupt&=~0x80000000;
-			//psxTestHWInts();
-		//}
+		if (psxRegs.interrupt & 0x80000000) {
+			psxRegs.interrupt&=~0x80000000;
+			psxTestHWInts();
+		}
 	}
-	psxTestHWInts();
+	//psxTestHWInts();
 //	if (psxRegs.cycle > 0xd29c6500) Log=1;
 }
 
