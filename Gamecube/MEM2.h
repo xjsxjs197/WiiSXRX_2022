b7cf63eab7e5367421aba4b51bf7222ce09b8085
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2024-03-20 19:14:29 -0500

    tidy up some MEM2 usage comments

--- Gamecube/MEM2.h
+++ Gamecube/MEM2.h
@@ -5,7 +5,7 @@
 #ifndef MEM2_H
 #define MEM2_H
 
-// Define a MegaByte
+// Define a KiloByte (KB) and a MegaByte (MB)
 #define KB (1024)
 #define MB (1024*1024)
 
@@ -29,17 +29,17 @@
 #define CN_FONT_LO   (MCD2_HI)
 #define CN_FONT_HI   (CN_FONT_LO + CN_FONT_SIZE)
 
-// We want 10MB for the recompiled blocks of DYNAREC
+// We want 10MB for the recompiled blocks of the 'new' PPC Dynarec
 #define RECMEM2_SIZE (10*MB)
 #define RECMEM2_LO   (CN_FONT_HI)
 #define RECMEM2_HI   (RECMEM2_LO + RECMEM2_SIZE)
 
-// We want 512KB for spu buffer
+// We want 512KB for the SPU buffer
 #define SPU_BUF_SIZE (512*KB)
 #define SPU_BUF_LO   (RECMEM2_HI)
 #define SPU_BUF_HI   (SPU_BUF_LO + SPU_BUF_SIZE)
 
-// We want 4M for Lightrec buffer
+// We want 4MB for Lightrec code buffer
 #define LIGHTREC_BUF_SIZE (4*MB)
 #define LIGHTREC_BUF_LO   (SPU_BUF_HI)
 #define LIGHTREC_BUF_HI   (LIGHTREC_BUF_LO + LIGHTREC_BUF_SIZE)
