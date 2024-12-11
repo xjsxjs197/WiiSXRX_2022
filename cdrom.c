7da47ac86c3de621c63294f540469ae493a3228f
xjsxjs197 <xiao.jiansheng@sysystem.com.cn>
2024-12-11 11:05:02 +0800

    Restore TurboCD to FastLoad
    TurboCD can cause many games to malfunction, but FastLoad can at least ensure that most of them can run normally. However, it seems that few games require FastLoad. In the future, I wii try other ways to implement FastLoad, such as multithreading

--- cdrom.c
+++ cdrom.c
@@ -678,14 +678,14 @@ static void cdrPlayInterrupt_Autopause(s16* cddaBuf)
 }
 
 
-static bool canDoTurbo(void)
+/*static bool canDoTurbo(void)
 {
 	u32 c = psxRegs.cycle;
 	return fastLoad && !cdr.RetryDetected && !cdr.AdpcmActive
 		//&& c - psxRegs.intCycle[PSXINT_SPUDMA].sCycle > (u32)cdReadTime * 2
 		&& c - psxRegs.intCycle[PSXINT_MDECOUTDMA].sCycle > (u32)cdReadTime * 16;
 }
-
+*/
 
 static int cdrSeekTime(unsigned char *target)
 {
@@ -1120,9 +1120,16 @@ void cdrInterrupt(void) {
 			}
 			else
 			{
+				if (fastLoad)
+				{
+					second_resp_time = cdReadTime;
+				}
+				else
+				{
 					second_resp_time = 2100011;
-					// a hack to try to avoid weird cmd vs irq1 races causing games to retry
-					second_resp_time += (cdr.RetryDetected & 15) * 100001;
+				}
+				// a hack to try to avoid weird cmd vs irq1 races causing games to retry
+				second_resp_time += (cdr.RetryDetected & 15) * 100001;
 			}
 			SetPlaySeekRead(cdr.StatP, 0);
 			DriveStateOld = cdr.DriveState;
@@ -1249,8 +1256,14 @@ void cdrInterrupt(void) {
 			StopReading();
 			SetPlaySeekRead(cdr.StatP, STATUS_SEEK | STATUS_ROTATING);
 
-			if (!canDoTurbo())
+			if (fastLoad)
+			{
+				seekTime = WaitTime1st;
+			}
+			else
+			{
 				seekTime = cdrSeekTime(cdr.SetSector);
+			}
 			*((u32*)cdr.SetSectorPlay) = *((u32*)cdr.SetSector);
 			cdr.DriveState = DRIVESTATE_SEEK;
 			/*
@@ -1408,11 +1421,16 @@ void cdrInterrupt(void) {
 			cdr.DriveState = DRIVESTATE_SEEK;
 
 			cycles = (cdr.Mode & MODE_SPEED) ? cdReadTime : cdReadTime * 2;
+			if (fastLoad)
+			{
+				if (seekTime > MinSeekTime)
+				{
+					seekTime = MinSeekTime;
+				}
+			}
 			cycles += seekTime;
 			if (Config.hacks.cdr_read_timing)
 				cycles = cdrAlignTimingHack(cycles);
-			else if (canDoTurbo())
-				cycles = cdReadTime / 2;
 			CDRPLAYREAD_INT(cycles, 1);
 
 			SetPlaySeekRead(cdr.StatP, STATUS_SEEK);
@@ -1671,8 +1689,8 @@ void cdrWrite1(unsigned char rt) {
 
 	if (!cdr.CmdInProgress) {
 		#ifdef DISP_DEBUG
-		sprintf(txtbuffer, "cdrCmd1 %s \r\n", CmdName[rt]);
-		DEBUG_print(txtbuffer, DBG_CDR1);
+		//sprintf(txtbuffer, "cdrCmd1 %s \r\n", CmdName[rt]);
+		//DEBUG_print(txtbuffer, DBG_CDR1);
 		//writeLogFile(txtbuffer);
 		#endif // DISP_DEBUG
 		cdr.CmdInProgress = rt;
@@ -1875,8 +1893,6 @@ void psxDma3(u32 madr, u32 bcr, u32 chcr) {
 				// halted
 				psxRegs.cycle += cycles - 20;
 			}
-			if (canDoTurbo() && cdr.Reading && cdr.FifoOffset >= 2048)
-				CDRPLAYREAD_INT(cycles + 4096, 1);
 			return;
 
 		default:
