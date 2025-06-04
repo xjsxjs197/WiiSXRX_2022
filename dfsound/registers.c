xjsxjs197/WiiSXRX_2022

commit 4b2db567e14286f71d212224709293b17a271380

avoid some left shifting to sign bit (notaz)
doesn't patch out left shifting negative values as that seems to be
properly handled by gcc/clang/msvc (even while the standard makes it
undefined), and patching it would uglify the code too much

https://github.com/libretro/pcsx_rearmed/commit/f707f14b867b499195a858cbedd80de88c761b5d

dfsound/registers.c
@@ -55,7 +55,7 @@ void CALLBACK DF_SPUwriteRegister(unsigned long reg, unsigned short val,
  int changed = spu.regArea[rofs] != val;
  spu.regArea[rofs] = val;
 
- if (!changed && (ignore_dupe[rofs >> 5] & (1 << (rofs & 0x1f))))
+ if (!changed && (ignore_dupe[rofs >> 5] & (1u << (rofs & 0x1f))))
   return;
  // zero keyon/keyoff?
  if (val == 0 && (r & 0xff8) == 0xd88)

gte.c
@@ -201,7 +201,7 @@ static inline s32 LIM_(psxCP2Regs *regs, s32 value, s32 max, s32 min, u32 flag) 
 
 static inline u32 limE_(psxCP2Regs *regs, u32 result) {
     if (result > 0x1ffff) {
-        gteFLAG |= (1 << 31) | (1 << 17);
+        gteFLAG |= (1u << 31) | (1u << 17);
         return 0x1ffff;
     }
     return result;
@@ -213,25 +213,21 @@ static inline u32 limE_(psxCP2Regs *regs, u32 result) {
 #define limE(result) \
     limE_(regs,result)
 
-#define A1(a) BOUNDS((a), 0x7fffffff, (1 << 30), -(s64)0x80000000, (1 << 31) | (1 << 27))
-#define A2(a) BOUNDS((a), 0x7fffffff, (1 << 29), -(s64)0x80000000, (1 << 31) | (1 << 26))
-#define A3(a) BOUNDS((a), 0x7fffffff, (1 << 28), -(s64)0x80000000, (1 << 31) | (1 << 25))
-#define limB1(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1 << 31) | (1 << 24))
-#define limB2(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1 << 31) | (1 << 23))
-#define limB3(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1 << 22))
-#define limC1(a) LIM((a), 0x00ff, 0x0000, (1 << 21))
-#define limC2(a) LIM((a), 0x00ff, 0x0000, (1 << 20))
-#define limC3(a) LIM((a), 0x00ff, 0x0000, (1 << 19))
-#define limD(a) LIM((a), 0xffff, 0x0000, (1 << 31) | (1 << 18))
+#define A1(a) BOUNDS((a), 0x7fffffff, (1u << 30), -(s64)0x80000000, (1u << 31) | (1u << 27))Add commentMore actions
+#define A2(a) BOUNDS((a), 0x7fffffff, (1u << 29), -(s64)0x80000000, (1u << 31) | (1u << 26))
+#define A3(a) BOUNDS((a), 0x7fffffff, (1u << 28), -(s64)0x80000000, (1u << 31) | (1u << 25))
+#define limB1(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1u << 31) | (1u << 24))
+#define limB2(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1u << 31) | (1u << 23))
+#define limB3(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1u << 22))
+#define limC1(a) LIM((a), 0x00ff, 0x0000, (1u << 21))
+#define limC2(a) LIM((a), 0x00ff, 0x0000, (1u << 20))
+#define limC3(a) LIM((a), 0x00ff, 0x0000, (1u << 19))
+#define limD(a) LIM((a), 0xffff, 0x0000, (1u << 31) | (1u << 18))
 
-#define F(a) BOUNDS((a), 0x7fffffff, (1 << 31) | (1 << 16), -(s64)0x80000000, (1 << 31) | (1 << 15))
-#define limG1(a) LIM((a), 0x3ff, -0x400, (1 << 31) | (1 << 14))
-#define limG2(a) LIM((a), 0x3ff, -0x400, (1 << 31) | (1 << 13))
-//Fix for Valkyrie Profile crash loading world map
-// (PCSX Rearmed commit 7384197d8a5fd20a4d94f3517a6462f7fe86dd4c
-//  'seems to work, unverified value')
-//#define limH(a) LIM((a), 0xfff, 0x000, (1 << 12))
-#define limH(a) LIM((a), 0x1000, 0x0000, (1 << 12))
+#define F(a) BOUNDS((a), 0x7fffffff, (1u << 31) | (1u << 16), -(s64)0x80000000, (1u << 31) | (1u << 15))
+#define limG1(a) LIM((a), 0x3ff, -0x400, (1u << 31) | (1u << 14))
+#define limG2(a) LIM((a), 0x3ff, -0x400, (1u << 31) | (1u << 13))
+#define limH(a) LIM((a), 0x1000, 0x0000, (1u << 12))
 
 #ifndef __arm__
 #define A1U A1
@@ -319,12 +315,12 @@ static inline void MTC2(u32 value, int reg) {
                 a = gteLZCS;
                 if (a > 0) {
                     int i;
-                    for (i = 31; (a & (1 << i)) == 0 && i >= 0; i--);
+                    for (i = 31; (a & (1u << i)) == 0 && i >= 0; i--);
                     gteLZCR = 31 - i;
                 } else if (a < 0) {
                     int i;
                     a ^= 0xffffffff;
-                    for (i = 31; (a & (1 << i)) == 0 && i >= 0; i--);
+                    for (i = 31; (a & (1u << i)) == 0 && i >= 0; i--);
                     gteLZCR = 31 - i;
                 } else {
                     gteLZCR = 32;

psxbios.c
@@ -336,7 +336,7 @@ static int card_io_delay;
 #define CARD_HARDLER_READM  0x5689 // fake, for psxBios_read()
 #define CARD_HARDLER_INFO   0x5B64
 
-#define HLEOP(n) SWAPu32((0x3b << 26) | (n));
+#define HLEOP(n) SWAPu32((0x3bu << 26) | (n));
 
 static u8 loadRam8(u32 addr)
 {

psxhw.h
@@ -57,14 +57,14 @@ extern "C" {
 #define HW_DMA_PCR   (psxHu32ref(0x10f0))
 #define HW_DMA_ICR   (psxHu32ref(0x10f4))
 
-#define HW_DMA_ICR_BUS_ERROR     (1<<15)
-#define HW_DMA_ICR_GLOBAL_ENABLE (1<<23)
-#define HW_DMA_ICR_IRQ_SENT      (1<<31)
+#define HW_DMA_ICR_BUS_ERROR     (1u << 15)
+#define HW_DMA_ICR_GLOBAL_ENABLE (1u << 23)
+#define HW_DMA_ICR_IRQ_SENT      (1u << 31)
 
 #define	DMA_INTERRUPT(n) \
     u32 icr = LOAD_SWAP32p(psxHAddr(0x10f4)); \
-	if (icr & (1 << (16 + n))) { \
-        icr |= 1 << (24 + n); \
+	if (icr & (1u << (16 + n))) { \
+        icr |= 1u << (24 + n); \
         if (icr & HW_DMA_ICR_GLOBAL_ENABLE && !(icr & HW_DMA_ICR_IRQ_SENT)) { \
 		    psxHu32ref(0x1070) |= SWAP32(8); \
 		    icr |= HW_DMA_ICR_IRQ_SENT; \

psxinterpreter.c
@@ -875,7 +875,7 @@ void MTC0(int reg, u32 val) {
 			    DEBUG_print(txtbuffer, DBG_CDR3);
 			}
 			#endif // DISP_DEBUG
-			if (unlikely((psxRegs.CP0.n.SR ^ val) & (1 << 16)))
+			if (unlikely((psxRegs.CP0.n.SR ^ val) & (1u << 16)))
 				psxMemOnIsolate((val >> 16) & 1);
 			psxRegs.CP0.r[12] = val;
 			psxTestSWInts(1);

commit cf3e5f69da09ba179a1173688d250724bd5c354c

revert 4b2db567e14286f71d212224709293b17a271380
broke some games with this commit.

dfsound/registers.c
@@ -55,7 +55,7 @@ void CALLBACK DF_SPUwriteRegister(unsigned long reg, unsigned short val,
  int changed = spu.regArea[rofs] != val;
  spu.regArea[rofs] = val;
 
- if (!changed && (ignore_dupe[rofs >> 5] & (1u << (rofs & 0x1f))))
+ if (!changed && (ignore_dupe[rofs >> 5] & (1 << (rofs & 0x1f))))
   return;
  // zero keyon/keyoff?
  if (val == 0 && (r & 0xff8) == 0xd88)

gte.c
@@ -201,7 +201,7 @@ static inline s32 LIM_(psxCP2Regs *regs, s32 value, s32 max, s32 min, u32 flag) 
 
 static inline u32 limE_(psxCP2Regs *regs, u32 result) {
     if (result > 0x1ffff) {
-        gteFLAG |= (1u << 31) | (1u << 17);
+        gteFLAG |= (1 << 31) | (1 << 17);
         return 0x1ffff;
     }
     return result;
@@ -213,21 +213,25 @@ static inline u32 limE_(psxCP2Regs *regs, u32 result) {
 #define limE(result) \
     limE_(regs,result)
 
-#define A1(a) BOUNDS((a), 0x7fffffff, (1u << 30), -(s64)0x80000000, (1u << 31) | (1u << 27))
-#define A2(a) BOUNDS((a), 0x7fffffff, (1u << 29), -(s64)0x80000000, (1u << 31) | (1u << 26))
-#define A3(a) BOUNDS((a), 0x7fffffff, (1u << 28), -(s64)0x80000000, (1u << 31) | (1u << 25))
-#define limB1(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1u << 31) | (1u << 24))
-#define limB2(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1u << 31) | (1u << 23))
-#define limB3(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1u << 22))
-#define limC1(a) LIM((a), 0x00ff, 0x0000, (1u << 21))
-#define limC2(a) LIM((a), 0x00ff, 0x0000, (1u << 20))
-#define limC3(a) LIM((a), 0x00ff, 0x0000, (1u << 19))
-#define limD(a) LIM((a), 0xffff, 0x0000, (1u << 31) | (1u << 18))
+#define A1(a) BOUNDS((a), 0x7fffffff, (1 << 30), -(s64)0x80000000, (1 << 31) | (1 << 27))
+#define A2(a) BOUNDS((a), 0x7fffffff, (1 << 29), -(s64)0x80000000, (1 << 31) | (1 << 26))
+#define A3(a) BOUNDS((a), 0x7fffffff, (1 << 28), -(s64)0x80000000, (1 << 31) | (1 << 25))
+#define limB1(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1 << 31) | (1 << 24))
+#define limB2(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1 << 31) | (1 << 23))
+#define limB3(a, l) LIM((a), 0x7fff, -0x8000 * !l, (1 << 22))
+#define limC1(a) LIM((a), 0x00ff, 0x0000, (1 << 21))
+#define limC2(a) LIM((a), 0x00ff, 0x0000, (1 << 20))
+#define limC3(a) LIM((a), 0x00ff, 0x0000, (1 << 19))
+#define limD(a) LIM((a), 0xffff, 0x0000, (1 << 31) | (1 << 18))
 
-#define F(a) BOUNDS((a), 0x7fffffff, (1u << 31) | (1u << 16), -(s64)0x80000000, (1u << 31) | (1u << 15))
-#define limG1(a) LIM((a), 0x3ff, -0x400, (1u << 31) | (1u << 14))
-#define limG2(a) LIM((a), 0x3ff, -0x400, (1u << 31) | (1u << 13))
-#define limH(a) LIM((a), 0x1000, 0x0000, (1u << 12))
+#define F(a) BOUNDS((a), 0x7fffffff, (1 << 31) | (1 << 16), -(s64)0x80000000, (1 << 31) | (1 << 15))
+#define limG1(a) LIM((a), 0x3ff, -0x400, (1 << 31) | (1 << 14))
+#define limG2(a) LIM((a), 0x3ff, -0x400, (1 << 31) | (1 << 13))
+//Fix for Valkyrie Profile crash loading world map
+// (PCSX Rearmed commit 7384197d8a5fd20a4d94f3517a6462f7fe86dd4c
+//  'seems to work, unverified value')
+//#define limH(a) LIM((a), 0xfff, 0x000, (1 << 12))
+#define limH(a) LIM((a), 0x1000, 0x0000, (1 << 12))
 
 #ifndef __arm__
 #define A1U A1
@@ -315,12 +319,12 @@ static inline void MTC2(u32 value, int reg) {
                 a = gteLZCS;
                 if (a > 0) {
                     int i;
-                    for (i = 31; (a & (1u << i)) == 0 && i >= 0; i--);
+                    for (i = 31; (a & (1 << i)) == 0 && i >= 0; i--);
                     gteLZCR = 31 - i;
                 } else if (a < 0) {
                     int i;
                     a ^= 0xffffffff;
-                    for (i = 31; (a & (1u << i)) == 0 && i >= 0; i--);
+                    for (i = 31; (a & (1 << i)) == 0 && i >= 0; i--);
                     gteLZCR = 31 - i;
                 } else {
                     gteLZCR = 32;

psxbios.c
@@ -336,7 +336,7 @@ static int card_io_delay;
 #define CARD_HARDLER_READM  0x5689 // fake, for psxBios_read()
 #define CARD_HARDLER_INFO   0x5B64
 
-#define HLEOP(n) SWAPu32((0x3bu << 26) | (n));
+#define HLEOP(n) SWAPu32((0x3b << 26) | (n));
 
 static u8 loadRam8(u32 addr)
 {

psxhw.h
@@ -57,14 +57,14 @@ extern "C" {
 #define HW_DMA_PCR   (psxHu32ref(0x10f0))
 #define HW_DMA_ICR   (psxHu32ref(0x10f4))
 
-#define HW_DMA_ICR_BUS_ERROR     (1u << 15)
-#define HW_DMA_ICR_GLOBAL_ENABLE (1u << 23)
-#define HW_DMA_ICR_IRQ_SENT      (1u << 31)
+#define HW_DMA_ICR_BUS_ERROR     (1<<15)
+#define HW_DMA_ICR_GLOBAL_ENABLE (1<<23)
+#define HW_DMA_ICR_IRQ_SENT      (1<<31)
 
 #define	DMA_INTERRUPT(n) \
     u32 icr = LOAD_SWAP32p(psxHAddr(0x10f4)); \
-	if (icr & (1u << (16 + n))) { \
-        icr |= 1u << (24 + n); \
+	if (icr & (1 << (16 + n))) { \
+        icr |= 1 << (24 + n); \
         if (icr & HW_DMA_ICR_GLOBAL_ENABLE && !(icr & HW_DMA_ICR_IRQ_SENT)) { \
 		    psxHu32ref(0x1070) |= SWAP32(8); \
 		    icr |= HW_DMA_ICR_IRQ_SENT; \

psxinterpreter.c
@@ -875,7 +875,7 @@ void MTC0(int reg, u32 val) {
 			    DEBUG_print(txtbuffer, DBG_CDR3);
 			}
 			#endif // DISP_DEBUG
-			if (unlikely((psxRegs.CP0.n.SR ^ val) & (1u << 16)))
+			if (unlikely((psxRegs.CP0.n.SR ^ val) & (1 << 16)))
 				psxMemOnIsolate((val >> 16) & 1);
 			psxRegs.CP0.r[12] = val;
 			psxTestSWInts(1);